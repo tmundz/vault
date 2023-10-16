72c722d546b09f92452c7dd5276dce1427550a30
Travis Mundy <mundyt4@mymacewan.ca>
2023-10-16 17:40:16 -0600

    update go mod

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  //"fmt"
+  //"log"
+  "github.com/gofiber/fiber/v2"
+  
+)
+
+
+func main() {
+  app := fiber.New()
+  app.Get("/", func(c * fiber.Ctx) error {
+    return c.SendString("Hello")
+  })
+  app.Listen(":8080")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module vault
+module github.com/tmundz/vault
 
-go 1.21.1
+go 1.21.2
